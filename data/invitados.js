7d8538cb552f91fe94713123adc10880687c0128
DrojasFrontend <daniel.rojas@leggercolombia.com>
2025-02-10 01:58:10 -0500

    invitados

--- data/invitados.js
+++ data/invitados.js
@@ -1,20 +1,836 @@
 const invitados = [
   {
-    nombre: "Daniel Rojas",
-    eventos: 2,
+    nombre: "Rodrigo Bravo",
+    eventos: 3,
     confirmado: false,
-    acompanantes: [
-      "Maria Rojas",
-      "Juan Rojas"
-    ]
+    acompanantes: ["Megan Opazo"]
   },
   {
-    nombre: "Liliana",
-    eventos: 2,
+    nombre: "Ruby Aldea",
+    eventos: 3,
     confirmado: false,
-    acompanantes: [
-      "Liliana",
-      "Alfredo"
-    ]
+    acompanantes: ["Nayid"]
+  },
+  {
+    nombre: "Ana Paula Chavez",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Jorge Chavez"]
+  },
+  {
+    nombre: "Brenda Contreras",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Invitado"]
+  },
+  {
+    nombre: "Valeria Tuesta",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Russell Lavado"]
+  },
+  {
+    nombre: "Diego Diaz",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Sofia Otarola"]
+  },
+  {
+    nombre: "Michelle Rojas",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Elizabeth K. Avila"]
+  },
+  {
+    nombre: "Daniela Nacarino",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Fabricio Estupiñan", "Erick Allende"]
+  },
+  {
+    nombre: "Liliana Rojas",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Invitado"]
+  },
+  {
+    nombre: "Geraldine Alvarado",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Oscar Aledo"]
+  },
+  {
+    nombre: "Mauricio Valdivia",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Andrea Chavez"]
+  },
+  {
+    nombre: "Katherine Grandez",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Tomas Luciani"]
+  },
+  {
+    nombre: "Daniel Nacarino",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Rosa Tovar"]
+  },
+  {
+    nombre: "David Lizama",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Elena Santillan"]
+  },
+  {
+    nombre: "Pilar Chirinos",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Augusto Echegaray"]
+  },
+  {
+    nombre: "Kenji Lopez",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Ale Lopez"]
+  },
+  {
+    nombre: "Carol Icaza",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Cesar Andres"]
+  },
+  {
+    nombre: "Arlette Moarri",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Fernando Jibaja"]
+  },
+  {
+    nombre: "Anthony Romero",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Tamy Cruz"]
+  },
+  {
+    nombre: "Elia Samame",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Dennis Delgado"]
+  },
+  {
+    nombre: "Francesca Chappell",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Diego Best"]
+  },
+  {
+    nombre: "Mary Sobrados",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Martin Luque"]
+  },
+  {
+    nombre: "Lady Espinoza",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Frank Anderson"]
+  },
+  {
+    nombre: "Manuel Torres",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Heder"]
+  },
+  {
+    nombre: "Paola Buczynski",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Mike Buczynski", "Mauricio Nacarino"]
+  },
+  {
+    nombre: "Karen Zannelli",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Alvaro Zannelli"]
+  },
+  {
+    nombre: "Karen Navarra",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Kevin Navarra"]
+  },
+  {
+    nombre: "Anapaula Satiesteban",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Invitado"]
+  },
+  {
+    nombre: "Michael Busler",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Katherine Busler"]
+  },
+  {
+    nombre: "Adam Busler",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Jennifer Busler"]
+  },
+  {
+    nombre: "Kaitlyn DeBernardo",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Ralph DeBernardo"]
+  },
+  {
+    nombre: "Veronica Moriarity",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Dave Moriarity"]
+  },
+  {
+    nombre: "Mary Beth Moriarity",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Paddy Moriarity"]
+  },
+  {
+    nombre: "Dean Moriarity",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Karianne Tremblay"]
+  },
+  {
+    nombre: "Dylan Moriarity",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Ashley Galvan"]
+  },
+  {
+    nombre: "Justin Gentile",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Michelle Del Valle"]
+  },
+  {
+    nombre: "Justin Rau",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Gabrielle Beach"]
+  },
+  {
+    nombre: "Michael McCarron",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Raina Roche"]
+  },
+  {
+    nombre: "Joe Marciani",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Tina Marchiani"]
+  },
+  {
+    nombre: "Isabel Tyler",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Tristan Tyler"]
+  },
+  {
+    nombre: "Sal Imperiale",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Carly Imperiale"]
+  },
+  {
+    nombre: "Adam Seiden",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Melissa Bennett"]
+  },
+  {
+    nombre: "Janey Goldberg",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Andrew Steinmetz"]
+  },
+  {
+    nombre: "Matt Beirne",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Danielle Naoum"]
+  },
+  {
+    nombre: "Joe Cappocci",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Kristy Haverstick"]
+  },
+  {
+    nombre: "Jackie Masters",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Joe Masters"]
+  },
+  {
+    nombre: "Keith Corcoran",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Emily Corcoran"]
+  },
+  {
+    nombre: "Dan Lombardi",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Montana Lombardi"]
+  },
+  {
+    nombre: "Dave Hubsher",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Ali Hubsher"]
+  },
+  {
+    nombre: "Mike Boyle",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Nadia Boyle"]
+  },
+  {
+    nombre: "Mike Bagnoli",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Laura Bagnoli"]
+  },
+  {
+    nombre: "Colin Dampier",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Anna Dampier"]
+  },
+  {
+    nombre: "Frank Diebold",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Chelsea Prior"]
+  },
+  {
+    nombre: "Gage O'Connell",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Megan O'Connell"]
+  },
+  {
+    nombre: "Sam Biddle",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Melanie Newton"]
+  },
+  {
+    nombre: "Matt Wetherell",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Cassie Wetherell"]
+  },
+  {
+    nombre: "Zack Matheson",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Emily Matheson"]
+  },
+  {
+    nombre: "Justin Davis",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Chelsea Davis"]
+  },
+  {
+    nombre: "John Savage",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Tatum Savage"]
+  },
+  {
+    nombre: "Matt Andrews",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Meghan Karoly"]
+  },
+  {
+    nombre: "Rob Moir",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Aubrey Moir"]
+  },
+  {
+    nombre: "Chris Irwin",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Kellyanne Miller"]
+  },
+  {
+    nombre: "Armando Urias",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Megan Urias"]
+  },
+  {
+    nombre: "Joe Stassi",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Cassie Stassie"]
+  },
+  {
+    nombre: "Elliot Whelan",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Hollis Heydenreich"]
+  },
+  {
+    nombre: "Mac Harris",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Liz Gullett"]
+  },
+  {
+    nombre: "Carlo Candela",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Jaclyn Candela"]
+  },
+  {
+    nombre: "Mike Magee",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Lauren Peck"]
+  },
+  {
+    nombre: "Kevin Moyer",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Gina Moyer"]
+  },
+  {
+    nombre: "Emily Rossum",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Jordan Rossum"]
+  },
+  {
+    nombre: "Michelle Gardner",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Norm Imber"]
+  },
+  {
+    nombre: "Dave Gardner",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Paula Gardner"]
+  },
+  {
+    nombre: "Nick DePhillipo",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Valerie DePhillipo"]
+  },
+  {
+    nombre: "Matt Reece",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Jordan Reece"]
+  },
+  {
+    nombre: "Andrew Mcelroy",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Amanda McElroy"]
+  },
+  {
+    nombre: "Becca Linquist",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Taylor Udell"]
+  },
+  {
+    nombre: "Pat Schall",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Melissa Schall"]
+  },
+  {
+    nombre: "Chris Mancuso",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Abigail Gragg"]
+  },
+  {
+    nombre: "Peter Longo",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Lindsey Longo"]
+  },
+  {
+    nombre: "Peter Lacovara",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Sarah Lewis"]
+  },
+  {
+    nombre: "Paddy Bruder",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Doug Brown"]
+  },
+  {
+    nombre: "Meghan Bush",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["John Bush"]
+  },
+  {
+    nombre: "Renee Cronin",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Kyle Cronin"]
+  },
+  {
+    nombre: "Dan Lowenherz",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Rachel Lowenherz"]
+  },
+  {
+    nombre: "Patrick Sofen",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Stef Corgel"]
+  },
+  {
+    nombre: "Frank Tumarello",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Kelly Mackley"]
+  },
+  {
+    nombre: "Patrick Smith",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Frannie Smith"]
+  },
+  {
+    nombre: "Bob Bechtel",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Lauren Bechtel"]
+  },
+  {
+    nombre: "Matt Pellegrino",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Delsa Pellegrino"]
+  },
+  {
+    nombre: "Mike Garafolo",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Brittney Garafolo"]
+  },
+  {
+    nombre: "Mario Lolo Macedo",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Luz Mery Macedo"]
+  },
+  {
+    nombre: "Marjorie Berrocal",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Alex Ramos"]
+  },
+  {
+    nombre: "Maite Peralta",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Andre Carrion"]
+  },
+  {
+    nombre: "Steph Ortiz",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Joe Callahan"]
+  },
+  {
+    nombre: "Victoria Balara",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["John Ventriglia"]
+  },
+  {
+    nombre: "Bobby Bechtel",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Lauren Genova"]
+  },
+  {
+    nombre: "Patrick Smith",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: ["Frannie Smith"]
+  },
+  {
+    nombre: "Sean McGonical",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Carl Harris",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Bryan Sorenson",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Lauren Dalzell",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Yolanda Vasquez",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Patricia Carhuatocto",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Gianina La Serna",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Fernando Gutierrez",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Eithel Cordova",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Cyntia Romero",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Nataly Otoya",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Luisa Benazar",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Desiree Seminario",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Claudia Garcia",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Bryan Campos",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Kevin Silva",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Jaime Huaman",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Fiorella Villalobos",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Wendy Angeles",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Maximiliano Nacarino",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Jimmy Gentile",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Jim Abbott",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "AJ Holland",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Jeremy Glick",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Chris Ruley",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Ryan Rothstein",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Nate Segal",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Bill Zogorski",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Carl Harris",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Matt Eisenstein",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Bryan Sorenson",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Patrick Schell",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Stephen Ruff",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Joe Hutchinson",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Mary Jane Breen",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Joan Hurley",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Rick Guerrieri",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Marya Macedo",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Carl Harris",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Bryan Sorensen",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Romyna Flores",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
+  },
+  {
+    nombre: "Kelly Peralta",
+    eventos: 3,
+    confirmado: false,
+    acompanantes: []
   }
 ];
