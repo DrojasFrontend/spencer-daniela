c048f8796e7eeb98be46d87a8b9970c1a588670b
DrojasFrontend <daniel.rojas@leggercolombia.com>
2025-02-16 11:56:52 -0500

    eliminar zoom

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
   <!-- compatability header includes -->
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
   <!-- open graph header includes -->
   <meta property="og:title" content="<?=get_the_title()?>" />
